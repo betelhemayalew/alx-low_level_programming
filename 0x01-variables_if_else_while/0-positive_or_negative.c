d1551449bfd5b4106754b5771ba0a71cea59b5bf
betelhemayalew <betelhemayalew60@gmail.com>
2023-03-19 08:04:30 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,14 +15,15 @@ if (n>0)
 {
 printf("%d is positive\n",n);
 }
-else if
+else if (n < 0)
+{
+printf("%d is negative\n",n);
+}
+else
 {
 printf("%d is zero\n",n);
 }
-else (n<0)
-{
-printf("%d is negative \n",n);
-}
 return(0);
 }
+
 
