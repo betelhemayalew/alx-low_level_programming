4791a6f096c1e676b62e0244e38e062bdf57d6df
betelhemayalew <betelhemayalew60@gmail.com>
2023-03-22 06:48:09 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include <_putchar.h>
+#include "main.h"
+
 /**
- *main: print for the
- *return : 0
- */
-int main (void)
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
 {
-_putchar("_putchar")
-return: 0
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+return (0);
 }
-
-
