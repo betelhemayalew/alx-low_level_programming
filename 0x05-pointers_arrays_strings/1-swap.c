4a7381b23cb84b7181dc017aad532bd703d28832
betelhemayalew <betelhemayalew60@gmail.com>
2023-03-27 11:00:10 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -11,11 +11,9 @@ int main(void)
     int a;
     int b;
 
-void swap_int(int *a, int *b)
-{
-	int changed;
-
-	changed = *a;
-	*a = *b;
-	*b = changed;
+    a = 98;
+    b = 42;
+    void swap_int(int *a, int *b);("a=%d, b=%d\n", a, b);
+    swap_int("a=%d, b=%d\n", a, b);
+    return (0);
 }
