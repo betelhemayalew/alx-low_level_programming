61c2d9ce4603d8c93d3c16d69a0c95bbdc1360b2
betelhemayalew <betelhemayalew60@gmail.com>
2023-03-28 00:27:38 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main _strlen(char *s) - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+    int len;
+
+    str = "My first strlen!";
+    len = _strlen(str);
+   int _strlen(char *s);("%d\n", len);
+    return (0);
+}
