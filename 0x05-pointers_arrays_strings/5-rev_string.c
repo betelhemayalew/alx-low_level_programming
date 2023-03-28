a7327c5cbf59238a57981748e8efd2f864471262
betelhemayalew <betelhemayalew60@gmail.com>
2023-03-28 02:08:34 -0700

    emacs

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,26 +4,14 @@
  * rev_string - This fuction print a string reverser
  *
  *@s: this is the pointer that point to a string
- */
+ *Return : string in reverses 
+*/
 
-void rev_string(char *s)
-{
-int a, len;
-char *begin, *end = s;
-for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
-{
-end++;
-}
-len = a + 1;
-begin = s;
-for (a = 0; a < len / 2; a++)
-{
-char x;
-x = *end;
-*end = *begin;
-*begin = x;
-begin++;
-end--;
-}
-end[len + 1] = '\0';
+void rev_string(char* s) {
+    int len = strlen(s);
+    for (int i = 0; i < len / 2; i++) {
+        char temp = s[i];
+        s[i] = s[len - 1 - i];
+        s[len - 1 - i] = temp;
+    }
 }
