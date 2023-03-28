742b58efa06da3076cbdc5625ab41e0a3dc3bdd3
betelhemayalew <betelhemayalew60@gmail.com>
2023-03-28 00:56:12 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  *_strlen - check the code
  *
  * Return: Always 0.
  */
-int _strlen(char)
+int _strlen(char *s)
 {
 int len = 0;
 while (*s != '\0')
@@ -16,5 +15,4 @@ s++;
 }
 return (len);
 }
-
 
