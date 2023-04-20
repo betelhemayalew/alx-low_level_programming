37220232e2c58fbde9862f1dfdb0c61a73fdc78c
betelhemayalew <betelhemayalew60@gmail.com>
2023-04-20 03:15:14 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,37 +1,20 @@
 #include "function_pointers.h"
 #include <stdio.h>
-/**
-* array_iterator - execute element of an array.
-* @array: array of integers.
-* @size: size of the array.
-* @action: pointer to the function you need to use.
-* Return: void.
-*/
-void array_iterator(int *array, size_t size, void (*action)(int))
-{
-for (size_t i = 0; i < size; i++)
-action(array[i]);
-}
-/**
- * print_int - prints an integer.
- * @n: integer to print.
- *
- * Return: void.
- */
-void print_int(int n)
-{
-printf("%d ", n);
-}
 
 /**
- * main - entry point.
- *
- * Return: 0 on success.
+ * array_iterator - prints each array elem on a newl
+ * @array: array
+ * @size: how many elem to print
+ * @action: pointer to print in regular or hex
+ * Return: void
  */
-int main(void)
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
-int arr[] = {1, 2, 3, 4, 5};
-size_t size = sizeof(arr) / sizeof(arr[0]);
-array_iterator(arr, size, print_int);
-return (0);
+unsigned int i;
+if (array == NULL || action == NULL)
+return;
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
 }
