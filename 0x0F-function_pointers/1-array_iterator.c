d59864d443d021e344ff508c8c1624250c3958ae
betelhemayalew <betelhemayalew60@gmail.com>
2023-04-20 03:06:04 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,22 +1,17 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
-#include <stdio.h>
-
 /**
- * array_iterator - executes a function given as a parameter on each element of an array.
- * @array: array of integers.
- * @size: size of the array.
- * @action: pointer to the function you need to use.
- *
- * Return: void.
- */
+* array_iterator - execute element of an array.
+* @array: array of integers.
+* @size: size of the array.
+* @action: pointer to the function you need to use.
+* Return: void.
+*/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    for (size_t i = 0; i < size; i++)
-        action(array[i]);
+for (size_t i = 0; i < size; i++)
+action(array[i]);
 }
-
 /**
  * print_int - prints an integer.
  * @n: integer to print.
@@ -25,7 +20,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
  */
 void print_int(int n)
 {
-    printf("%d ", n);
+printf("%d ", n);
 }
 
 /**
@@ -35,10 +30,8 @@ void print_int(int n)
  */
 int main(void)
 {
-    int arr[] = {1, 2, 3, 4, 5};
-    size_t size = sizeof(arr) / sizeof(arr[0]);
-
-    array_iterator(arr, size, print_int);
-
-    return (0);
+int arr[] = {1, 2, 3, 4, 5};
+size_t size = sizeof(arr) / sizeof(arr[0]);
+array_iterator(arr, size, print_int);
+return (0);
 }
